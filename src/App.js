28d811f1eeda066ccd333715ec2cb4bc461ecae7
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-09-19 23:09:04 +0600

    Create multiple components from an array of objects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+const singers = [
+  { name: 'Dr. Mahfuz', song: 'Khali Pata' },
+  { name: 'Habib Wahid', song: 'Din Gelo' },
+  { name: 'Asif Akbar', song: 'O Priya' },
+  { name: 'Hridoy Khan', song: 'Mayabi' },
+];
 function App() {
+  const heros = ['Rubel', 'Bappa', 'Kuber', 'Jashim', 'Salman Shah', 'Razzak'];
   return (
     <div className="App">
-      <Person name="Rubel" nayika="Moushumi"></Person>
-      <Person name="Bappa" nayika="Cheka"></Person>
-      <Person name="Kuber" nayika="Kopila"></Person>
+      {heros.map((hero) => (
+        <Person name={hero}></Person>
+      ))}
+      {
+        singers.map(singer => <Person name = {singer.name} song={singer.song}></Person>)
+      }
+
+      {/* <Person name={heros[0]} nayika="Moushumi"></Person>
+      <Person name={heros[1]} nayika="Cheka"></Person>
+      <Person name={heros[2]} nayika="Kopila"></Person>  */}
       <h5>New Component</h5>
       <Friend movie="Singham" phone="8374857"></Friend>
       <Friend movie="Drishyam" phone="9900998"></Friend>
@@ -17,7 +31,7 @@ function Person(props) {
   return (
     <div className="person">
       <h1>{props.name}</h1>
-      <p>Nayika: {props.nayika}</p>
+      <p>{props.song}</p>
     </div>
   );
 }
